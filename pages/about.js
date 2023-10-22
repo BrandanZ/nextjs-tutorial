38a471f454437d5fba408c56c4f089a94e9993be
brandon <branthebuilder321@gmail.com>
2023-10-22 19:52:59 -0400

    Add about and ninjas folder

---
+++ pages/about.js
@@ -0,0 +1,23 @@
+const About = () => {
+  return (
+    <div>
+      <h1>
+        About
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
+          earum, ullam veniam deleniti illum facere minima adipisci et eveniet
+          saepe aut fugiat vitae enim illo, debitis quidem facilis repudiandae
+          quam!
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
+          earum, ullam veniam deleniti illum facere minima adipisci et eveniet
+          saepe aut fugiat vitae enim illo, debitis quidem facilis repudiandae
+          quam!
+        </p>
+      </h1>
+    </div>
+  );
+};
+
+export default About;
