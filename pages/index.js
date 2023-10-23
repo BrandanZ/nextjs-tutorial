d68bb4d6414a3cee091b990db00c4b928b7f85cc
brandon <branthebuilder321@gmail.com>
2023-10-23 15:43:21 -0400

    Add components to index page

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,15 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Navbar from "@/comps/Navbar";
+import Footer from "@/comps/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore earum,
@@ -19,6 +22,7 @@ export default function Home() {
         ullam veniam deleniti illum facere minima adipisci et eveniet saepe aut
         fugiat vitae enim illo, debitis quidem facilis repudiandae quam!
       </p>
+      <Footer />
     </div>
   );
 }
