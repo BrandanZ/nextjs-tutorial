ecd538c7a48fec08ce2f007ce8fbc67c34ae62ee
brandon <branthebuilder321@gmail.com>
2023-10-24 16:56:12 -0400

    Add header images and metadata

--- pages/about.js
+++ pages/about.js
@@ -1,20 +1,29 @@
+import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore earum,
-        ullam veniam deleniti illum facere minima adipisci et eveniet saepe aut
-        fugiat vitae enim illo, debitis quidem facilis repudiandae quam!
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore earum,
-        ullam veniam deleniti illum facere minima adipisci et eveniet saepe aut
-        fugiat vitae enim illo, debitis quidem facilis repudiandae quam!
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About </title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
+          earum, ullam veniam deleniti illum facere minima adipisci et eveniet
+          saepe aut fugiat vitae enim illo, debitis quidem facilis repudiandae
+          quam!
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
+          earum, ullam veniam deleniti illum facere minima adipisci et eveniet
+          saepe aut fugiat vitae enim illo, debitis quidem facilis repudiandae
+          quam!
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -8,21 +8,29 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore earum,
-        ullam veniam deleniti illum facere minima adipisci et eveniet saepe aut
-        fugiat vitae enim illo, debitis quidem facilis repudiandae quam!
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore earum,
-        ullam veniam deleniti illum facere minima adipisci et eveniet saepe aut
-        fugiat vitae enim illo, debitis quidem facilis repudiandae quam!
-      </p>
-      <Link href="/ninjas" className={styles.btn}>
-        See Ninja Listing
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
+          earum, ullam veniam deleniti illum facere minima adipisci et eveniet
+          saepe aut fugiat vitae enim illo, debitis quidem facilis repudiandae
+          quam!
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
+          earum, ullam veniam deleniti illum facere minima adipisci et eveniet
+          saepe aut fugiat vitae enim illo, debitis quidem facilis repudiandae
+          quam!
+        </p>
+        <Link href="/ninjas" className={styles.btn}>
+          See Ninja Listing
+        </Link>
+      </div>
+    </>
   );
 }
