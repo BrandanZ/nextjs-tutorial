ac92a101f19c78b3f41ca0a12b42214108d8b0c3
brandon <branthebuilder321@gmail.com>
2023-10-24 17:22:25 -0400

    Add ninjas list with fetch

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,7 +1,23 @@
-const Ninjas = () => {
+import styles from "../../styles/Ninjas.module.css";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
   return (
     <div>
       <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <h3 className={styles.single}>{ninja.name}</h3>
+        </div>
+      ))}
     </div>
   );
 };
